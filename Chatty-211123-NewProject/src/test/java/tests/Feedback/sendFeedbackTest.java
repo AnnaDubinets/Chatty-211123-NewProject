3892db7db5b1b40d64294d092227fc6be28e6ea8
Albina Khamitova <albina757@yahoo.com>
2024-09-11 12:47:34 +0200

    feedback test

---
+++ Chatty-211123-NewProject/src/test/java/tests/Feedback/sendFeedbackTest.java
@@ -0,0 +1,19 @@
+package tests.Feedback;
+
+import dto.FeedbackRequest;
+import dto.LoginRequest;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static tests.BaseTest.*;
+
+public class sendFeedbackTest {
+    @Test
+    public void contactUsTest() {
+        LoginRequest loginRequest = new LoginRequest("johnsmith@gmail.com", "qwerty0707");
+        Response response = postRequest("api/auth/login", 200, loginRequest);
+        String accessToken = response.body().jsonPath().getString("accessToken");
+        FeedbackRequest feedbackRequest = new FeedbackRequest("Harvey", "johnsmith@gmail.com", "great site");
+        Response response1 = postRequestWithAccessToken("api/feedback", 201, feedbackRequest, accessToken);
+    }
+}
